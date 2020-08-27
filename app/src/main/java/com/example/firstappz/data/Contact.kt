5b69a2c19277d77f87975cb1393022c9245f328a
CHARAN <songyakusa@gmail.com>
2020-08-28 03:32:31 +0700

    Create the Contact entity

---
+++ app/src/main/java/com/example/firstappz/data/Contact.kt
@@ -0,0 +1,16 @@
+package com.example.firstappz.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "contact_table")
+data class Contact (
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0L,
+
+    @ColumnInfo(name = "name")
+    var name: String = "unnamed",
+
+)
