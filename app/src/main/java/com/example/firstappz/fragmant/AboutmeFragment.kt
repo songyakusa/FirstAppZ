4169ddf949bb2598925b31be9a5d760bad1919d5
CHARAN <songyakusa@gmail.com>
2020-08-28 03:19:58 +0700

    Add an onClick handler

--- app/src/main/java/com/example/firstappz/fragmant/AboutmeFragment.kt
+++ app/src/main/java/com/example/firstappz/fragmant/AboutmeFragment.kt
@@ -1,4 +1,41 @@
 package com.example.firstappz.fragmant
 
-class AboutmeFragment {
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.firstappz.R
+import com.example.firstappz.databinding.FragmentAboutmeBinding
+
+class AboutmeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var binding = DataBindingUtil.inflate<FragmentAboutmeBinding>(
+            inflater,
+            R.layout.fragment_aboutme,
+            container,
+            false
+        )
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
+
 }
--- app/src/main/java/com/example/firstappz/fragmant/ContactFragment.kt
+++ app/src/main/java/com/example/firstappz/fragmant/ContactFragment.kt
@@ -1,4 +1,40 @@
 package com.example.firstappz.fragmant
 
-class ContactFragment {
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.firstappz.R
+import com.example.firstappz.databinding.FragmentContactBinding
+
+class ContactFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        var binding = DataBindingUtil.inflate<FragmentContactBinding>(
+            inflater,
+            R.layout.fragment_contact,
+            container,
+            false
+        )
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
 }
