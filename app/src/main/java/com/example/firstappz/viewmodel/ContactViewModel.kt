35e8d6101ecd1481c33bbc43e12ec6e8f5dcb07f
CHARAN <songyakusa@gmail.com>
2020-08-28 04:11:05 +0700

    Collect and display the data   Collect and display the data

--- app/src/main/java/com/example/firstappz/viewmodel/ContactViewModel.kt
+++ app/src/main/java/com/example/firstappz/viewmodel/ContactViewModel.kt
@@ -1,13 +1,66 @@
 package com.example.firstappz.viewmodel
 
 import android.app.Application
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+import androidx.core.text.HtmlCompat
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Transformations
+import com.example.firstappz.data.Contact
 import com.example.firstappz.data.ContactDao
 import com.example.firstappz.databinding.FragmentContactBinding
+import kotlinx.coroutines.*
 
 class ContactViewModel(
-    val contactDao: ContactDao,
+    private val contactDao: ContactDao,
     private val binding: FragmentContactBinding,
     application: Application
 ) : AndroidViewModel(application) {
+
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val contacts = contactDao.get()
+    val contactString = Transformations.map(contacts) { contacts ->
+        formatContact(contacts)
+    }
+
+    private fun formatContact(contact: List<Contact>): Spanned {
+        val sb = StringBuilder()
+        sb.apply {
+            //append(resources.getString(R.string.title))
+            contact.forEach {
+                append(it.id)
+                append(" : ")
+                append(it.name)
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    fun onContactAdd() {
+        uiScope.launch {
+            val newContact = Contact()
+            newContact.name = binding.editTextTextPersonName.text.toString()
+
+            insert(newContact)
+        }
+    }
+
+    private suspend fun insert(contact: Contact) {
+        withContext(Dispatchers.IO) {
+            contactDao.insert(contact)
+        }
+    }
+
 }
