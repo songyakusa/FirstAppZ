a2214dca1456f7058f10a5f6405757e3bfed7dca
CHARAN <songyakusa@gmail.com>
2020-08-28 03:34:07 +0700

    Create the DAO

---
+++ app/src/main/java/com/example/firstappz/data/ContactDao.kt
@@ -0,0 +1,14 @@
+package com.example.firstappz.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ContactDao {
+    @Insert
+    fun insert(contact: Contact)
+    @Query("SELECT * from contact_table")
+    fun get(): LiveData<List<Contact>>
+}
